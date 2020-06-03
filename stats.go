fe4a2db35c99ff020831fbbd8629b379bdcc98ad
duongoku <nguyenvubinhduong@gmail.com>
2020-06-03 18:56:52 +0700

    Update stats.go

--- stats.go
+++ stats.go
@@ -40,8 +40,50 @@ func StatsHandle(s *discordgo.Session, m *discordgo.MessageCreate) {
 	tmps = tmps + "\nHealth Points: " + strconv.Itoa(p.Hp)
 	tmps = tmps + "\nAttack: " + strconv.Itoa(p.Atk)
 	tmps = tmps + "\nDefense: " + strconv.Itoa(p.Def)
-	tmps = tmps + "\nEvasion: " + strconv.Itoa(p.Evasion)
-	tmps = tmps + "\nCritical Chance: " + strconv.Itoa(p.CritChance)
+	tmps = tmps + "\nEvasion: " + strconv.Itoa(p.Evasion) + "%"
+	tmps = tmps + "\nCritical Chance: " + strconv.Itoa(p.CritChance) + "%"
+
+	s.ChannelMessageSend(m.ChannelID, tmps)
+}
+
+func EquipmentHandle(s *discordgo.Session, m *discordgo.MessageCreate) {
+	content := strings.Split(m.Content, " ")
+	if len(content)>2 {
+		s.ChannelMessageSend(m.ChannelID, "Too many arguments")
+		return
+	}
+	if len(m.Mentions)>1 {
+		s.ChannelMessageSend(m.ChannelID, "You mentioned more than one person")
+		return
+	}
+	u := m.Author
+	if len(m.Mentions)==1 {
+		u = m.Mentions[0]
+	}
+
+	CheckPlayer(u)
+	p := users[u.ID]
+
+	tmps := (p.Name + "'s Equipment:")
+	for _, eqm := range p.Equipment {
+		tmps = tmps + "\n" + eqm.Name + ": "
+		if eqm.Hp > 0 {
+			tmps = tmps + "Hp+" + strconv.Itoa(eqm.Hp) + ", "
+		}
+		if eqm.Atk > 0 {
+			tmps = tmps + "Attack+" + strconv.Itoa(eqm.Atk) + ", "
+		}
+		if eqm.Def > 0 {
+			tmps = tmps + "Defense+" + strconv.Itoa(eqm.Def) + ", "
+		}
+		if eqm.Evasion > 0 {
+			tmps = tmps + "Evasion+" + strconv.Itoa(eqm.Evasion) + "%, "
+		}
+		if eqm.CritChance > 0 {
+			tmps = tmps + "Critical Chance+" + strconv.Itoa(eqm.CritChance) + "%, "
+		}
+		tmps = tmps[0:len(tmps)-2]
+	}
 
 	s.ChannelMessageSend(m.ChannelID, tmps)
 }
