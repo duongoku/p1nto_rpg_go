d972d088c48396226d360867af9cc53cab4c697a
Darthveloper21 <hienvungoc01@gmail.com>
2020-06-06 18:42:45 +0700

    Update help.go

--- help.go
+++ help.go
@@ -12,12 +12,12 @@ func HelpHandle(s *discordgo.Session, m *discordgo.MessageCreate) {
 	temp += "- 'combat + @playername' to duel with @playername\n"
 	temp += "- 'stats' to display your stats\n"
 	temp += "- 'equipment' to display your equipment and Equipment ID\n"
-	temp += "- 'equip' + {ID Inventory} to equip item(ID Inventory) for combat\n"
-	temp += "- 'unequip' + {ID Inventory} to unequip item(ID Inventory) and set it back to your inventory\n"
+	temp += "- 'equip' + {ID in Inventory} to equip item(ID in Inventory) for combat\n"
+	temp += "- 'unequip' + {ID in Inventory} to unequip item(ID in Inventory) and set it back to your inventory\n"
 	temp += "- 'shop' to go to shop and see shelves and Shop ID\n"
 	temp += "- 'buy' + {Shop ID} to buy item(Shop ID) and add it to your inventory\n"
-	temp += "- 'sell' + {ID Inventory} to sell item(ID Inventory) for money\n"
-	temp += "- 'inventory' to show your inventory and Item ID Inventory\n"
+	temp += "- 'sell' + {ID in Inventory} to sell item(ID in Inventory) for money\n"
+	temp += "- 'inventory' to show your inventory and Item ID in Inventory\n"
 	temp += "- 'help' to display this shit```\n"
 	s.ChannelMessageSend(m.ChannelID, temp)
 }
