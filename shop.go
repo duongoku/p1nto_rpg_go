93cfa179bebc3dae9fd925ce1a6acf597d2fb4ce
duongoku <nguyenvubinhduong@gmail.com>
2020-06-06 16:04:10 +0700

    Update shop.go

--- shop.go
+++ shop.go
@@ -8,19 +8,7 @@ import (
 )
 
 func ShopHandle(s *discordgo.Session, m *discordgo.MessageCreate){
-	temp := "```Value type in order: Hp, Price, Atk, Def, Evasion, CritChance\n"
-	for _, value := range items {
-		temp += value.Name + ": " + "\n"
-		temp += strconv.Itoa(value.Price) + " "
-		temp += strconv.Itoa(value.Hp) + " "
-		temp += strconv.Itoa(value.Atk) + " "
-		temp += strconv.Itoa(value.Def) + " "
-		temp += strconv.Itoa(value.Evasion) + " "
-		temp += strconv.Itoa(value.CritChance) + "\n"
-	}
-	temp += "```"
 	
-	s.ChannelMessageSend(m.ChannelID, temp)
 }
 
 func BuyHandle(s *discordgo.Session, m *discordgo.MessageCreate){
