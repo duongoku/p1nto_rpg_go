62edac15fe6bb74ee84ee416408f0e9c5980e85f
duongoku <nguyenvubinhduong@gmail.com>
2020-06-06 16:05:02 +0700

    Update shop.go

--- shop.go
+++ shop.go
@@ -6,15 +6,3 @@ import (
 	"fmt"
 	"strings"
 )
-
-func ShopHandle(s *discordgo.Session, m *discordgo.MessageCreate){
-	
-}
-
-func BuyHandle(s *discordgo.Session, m *discordgo.MessageCreate){
-
-}
-
-func SellHandle(s *discordgo.Session, m *discordgo.MessageCreate){
-	
-}
