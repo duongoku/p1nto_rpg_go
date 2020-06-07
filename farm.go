9d3c23eec8c2bc7aea7d7721f24c2d21d090abc8
duongoku <nguyenvubinhduong@gmail.com>
2020-06-07 22:57:26 +0700

    Add more monsters

--- farm.go
+++ farm.go
@@ -3,8 +3,7 @@ package p1nto
 import (
 	"strings"
 	"strconv"
-	"fmt"
-	// "math/rand"
+	// "fmt"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -26,14 +25,29 @@ type dungeon struct {
 }
 
 func InitNPC() {
-	NPCs[0] = &NPC{"Raptor", []int{0}, 10, 100, 30, 5, 1, 1}
-	NPCs[1] = &NPC{"Murk Wolf", []int{0}, 50, 200, 60, 8, 1, 1}
-	NPCs[2] = &NPC{"Krug", []int{0}, 100, 300, 80, 10, 1, 1}
-	NPCs[3] = &NPC{"Gromp", []int{0}, 200, 400, 100, 12, 1, 1}
+	NPCs[0] = &NPC{"Raptor", []int{0}, 10, 40, 8, 3, 1, 50}
+	NPCs[1] = &NPC{"Murk Wolf", []int{0}, 20, 50, 25, 5, 1, 1}
+	NPCs[2] = &NPC{"Krug", []int{0}, 25, 60, 15, 5, 1, 50}
+	NPCs[3] = &NPC{"Gromp", []int{0}, 35, 100, 30, 10, 5, 1}
+	NPCs[4] = &NPC{"Sentinel", []int{0}, 60, 100, 50, 12, 10, 10}
+	NPCs[5] = &NPC{"Brambleback", []int{0}, 100, 180, 50, 25, 15, 10}
+	NPCs[6] = &NPC{"Rift Scuttler", []int{0}, 150, 280, 30, 25, 20, 60}
+	NPCs[7] = &NPC{"Earth Dragon", []int{0}, 170, 300, 40, 30, 5, 15}
+	NPCs[8] = &NPC{"Fire Dragon", []int{0}, 210, 300, 70, 20, 22, 25}
+	NPCs[9] = &NPC{"Green Hell Dragon", []int{0}, 300, 450, 100, 40, 20, 29}
+	NPCs[10] = &NPC{"Omega Shenron", []int{0}, 420, 666, 150, 50, 33, 35}
+	NPCs[11] = &NPC{"Baron Nashor", []int{0}, 600, 700, 220, 70, 15, 33}
+	NPCs[12] = &NPC{"Roshan", []int{0}, 750, 888, 350, 100, 40, 33}
+	NPCs[13] = &NPC{"Lich King", []int{0}, 1250, 1000, 600, 150, 35, 40}
+	NPCs[14] = &NPC{"The Ultimate P1nto Conqueror", []int{0}, 3000, 2500, 1000, 333, 50, 50}
 }
 
 func InitDungeon() {
 	dungeons[0] = &dungeon{[]int{0, 1, 2, 3}, []int{50, 30, 15, 5}}
+	dungeons[1] = &dungeon{[]int{4, 5, 6}, []int{60, 35, 5}}
+	dungeons[2] = &dungeon{[]int{7, 8, 9, 10}, []int{40, 30, 20, 10}}
+	dungeons[3] = &dungeon{[]int{11, 12, 13}, []int{50, 30, 20}}
+	dungeons[4] = &dungeon{[]int{14}, []int{100}}
 }
 
 func HitNPC(s *discordgo.Session, m *discordgo.Message, p *player, n *NPC, hpp *int, hpn *int) bool {
@@ -56,8 +70,8 @@ func HitNPC(s *discordgo.Session, m *discordgo.Message, p *player, n *NPC, hpp *
 	if *hpn <= 0 {
 		m, _ = s.ChannelMessage(m.ChannelID, m.ID)
 		tmp = m.Content + "\n"
-		tmp += n.Name + "'s HP reached 0. " + p.Name + " defeated " + n.Name
-		tmp += ", " + p.Name + " got $" + strconv.Itoa(n.Bounty)
+		tmp += n.Name + "'s HP reached 0, " + p.Name + " defeated " + n.Name
+		tmp += "and claimed $" + strconv.Itoa(n.Bounty)
 		p.Money += n.Bounty
 		s.ChannelMessageEdit(m.ChannelID, m.ID, tmp)
 		return true
@@ -82,7 +96,7 @@ func HitNPC(s *discordgo.Session, m *discordgo.Message, p *player, n *NPC, hpp *
 	if *hpp <= 0 {
 		m, _ = s.ChannelMessage(m.ChannelID, m.ID)
 		tmp = m.Content + "\n"
-		tmp += p.Name + "'s HP reached 0. " + p.Name + " is defeated"
+		tmp += p.Name + "'s HP reached 0, " + p.Name + " is defeated"
 		s.ChannelMessageEdit(m.ChannelID, m.ID, tmp)
 		return true
 	}
@@ -91,7 +105,7 @@ func HitNPC(s *discordgo.Session, m *discordgo.Message, p *player, n *NPC, hpp *
 }
 
 func CombatNPC(s *discordgo.Session, m *discordgo.MessageCreate, p *player, n *NPC) {
-	tmpm, _ := s.ChannelMessageSend(m.ChannelID, p.Name + " found a " + n.Name)
+	tmpm, _ := s.ChannelMessageSend(m.ChannelID, "A wild **" + n.Name + "** appeared")
 
 	hpp := p.Hp
 	hpn := n.Hp
@@ -135,7 +149,7 @@ func FarmHandle(s *discordgo.Session, m *discordgo.MessageCreate) {
 		cnt += 1
 	}
 
-	fmt.Println("Found " + NPCs[dun.Monsters[cnt]].Name)
+	//fmt.Println("debug")
 
 	CombatNPC(s, m, players[m.Author.ID], NPCs[dun.Monsters[cnt]])
 }
